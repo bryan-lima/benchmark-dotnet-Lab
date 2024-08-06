ce2966eaa99ea048454eb7bbdc5900297e534237
Bryan <bryanlima200@outlook.com>
2024-08-06 20:56:44 -0300

    feat: Add StringSplitBenchmark

--- src/SimpleBenchmarks/Program.cs
+++ src/SimpleBenchmarks/Program.cs
@@ -1,10 +1,10 @@
-﻿using BenchmarkDotNet.Reports;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 using SimpleBenchmarks;
 using SimpleBenchmarks.Benchmarks;
 
 Console.WriteLine("\n\nStarting benchmarks...");
 
 BenchmarkRunner.Run<FirstBenchmark>();
+BenchmarkRunner.Run<StringSplitBenchmark>();
 
 Console.WriteLine("\n\nBenchmarks completed!");
---
+++ src/SimpleBenchmarks/StringSplitBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SimpleBenchmarks
+{
+    [MemoryDiagnoser]
+    public class StringSplitBenchmark
+    {
+        #region Private Fields
+
+        private readonly string _text = "1234-2024";
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        [Benchmark(Baseline = true)]
+        public string[] SplitWithHyphenChar()
+        {
+            return _text.Split('-');
+        }
+
+        [Benchmark]
+        public string[] SplitWithHyphenString()
+        {
+            return _text.Split("-");
+        }
+
+        #endregion Public Methods
+    }
+}
